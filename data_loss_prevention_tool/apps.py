858824fec453c0c8b165ed03bd641a80bb1ca154
Artem <romanenkoartem24051991@gmail.com>
2024-08-11 15:47:11 +0200

    Add project apps, rest_framework, requirements.txt.

---
+++ data_loss_prevention_tool/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DataLossPreventionToolConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'data_loss_prevention_tool'
